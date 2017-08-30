da59c76c6284f9353f23e96bdcb78b7f461228c0
Julia Tufts <juliatufts@gmail.com>
2017-08-29 21:52:59 -0300

    Add VRTK SDK Manager to Touch Scene, and basic controller input listener

---
+++ OneToMany/Assets/Scripts/Controller.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+/// <summary>
+/// Listens to Controller Events and sends effect events accordingly.
+/// </summary>
+public class Controller : MonoBehaviour {
+
+    void Start()
+    {
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            Debug.LogError("Missing VRTK_ControllerEvents component");
+            return;
+        }
+
+        // Set up controller callbacks
+        GetComponent<VRTK_ControllerEvents>().TriggerClicked += new ControllerInteractionEventHandler(DoTriggerClicked);
+        GetComponent<VRTK_ControllerEvents>().TriggerUnclicked += new ControllerInteractionEventHandler(DoTriggerUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadPressed);
+        GetComponent<VRTK_ControllerEvents>().TouchpadReleased += new ControllerInteractionEventHandler(DoTouchpadReleased);
+
+        GetComponent<VRTK_ControllerEvents>().ControllerEnabled += new ControllerInteractionEventHandler(DoControllerEnabled);
+        GetComponent<VRTK_ControllerEvents>().ControllerDisabled += new ControllerInteractionEventHandler(DoControllerDisabled);
+
+        // Might not end up using these
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+        GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
+
+        GetComponent<VRTK_ControllerEvents>().TriggerTouchStart += new ControllerInteractionEventHandler(DoTriggerTouchStart);
+        GetComponent<VRTK_ControllerEvents>().TriggerTouchEnd += new ControllerInteractionEventHandler(DoTriggerTouchEnd);
+    }
+
+    private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Controller on index '" + index + "' " + button + " has been " + action
+                + " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+    }
+
+    private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "pressed", e);
+    }
+
+    private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "released", e);
+    }
+
+    private void DoTriggerTouchStart(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "touched", e);
+    }
+
+    private void DoTriggerTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "untouched", e);
+    }
+
+    private void DoTriggerClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "clicked", e);
+    }
+
+    private void DoTriggerUnclicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "unclicked", e);
+    }
+
+    private void DoTouchpadPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TOUCHPAD", "pressed down", e);
+    }
+
+    private void DoTouchpadReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TOUCHPAD", "released", e);
+    }
+
+    private void DoTouchpadTouchStart(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TOUCHPAD", "touched", e);
+    }
+
+    private void DoTouchpadTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TOUCHPAD", "untouched", e);
+    }
+
+    private void DoControllerEnabled(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "CONTROLLER STATE", "ENABLED", e);
+    }
+
+    private void DoControllerDisabled(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "CONTROLLER STATE", "DISABLED", e);
+    }
+}
