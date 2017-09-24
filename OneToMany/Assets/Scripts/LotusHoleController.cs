13b02c8287db9c931527b66d70c773b88a7fe2bd
Julia Tufts <juliatufts@gmail.com>
2017-09-23 21:23:22 -0300

    Lotus hole prototype

---
+++ OneToMany/Assets/Scripts/LotusHoleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotusHoleController : MonoBehaviour {
+
+    public GameObject lotus;
+    public GameObject holeEffectPrefab;
+    GameObject currentEffect;
+    Material lotusMaterial;
+
+    void Start()
+    {
+        lotusMaterial = lotus.GetComponent<Renderer>().material;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+		if (other.transform.CompareTag("GameController"))
+		{
+			currentEffect = Instantiate(holeEffectPrefab, transform.position, transform.rotation);
+		}
+    }
+}
