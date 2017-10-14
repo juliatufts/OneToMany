fd1cf242b01b4dd56f080bc4af809eb0dede14c3
harrison <notallama@gmail.com>
2017-10-13 21:54:57 -0300

    glow shader fixes

--- OneToMany/Assets/Scripts/MaybeGarbage/EnableDepth.cs
+++ OneToMany/Assets/Scripts/MaybeGarbage/EnableDepth.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class EnableDepth : MonoBehaviour {
 
-	void Start ()
-    {
-        foreach (var cam in Camera.allCameras)
-        {
-            cam.depthTextureMode = DepthTextureMode.Depth;
+    static EnableDepth primary;
+    void Update() {
+        if(!primary || !primary.enabled) {
+            primary = this;
         }
-	}
 
+        if(primary == this) {
+            foreach(var cam in Camera.allCameras) {
+                if(!cam.GetComponent<EnableDepth>()) {
+                    cam.gameObject.AddComponent<EnableDepth>();
+                }
+            }
+        }
+
+        var camera = GetComponent<Camera>();
+        if(camera) {
+            camera.depthTextureMode |= DepthTextureMode.Depth;
+        }
+    }
+
+    void OnPreRender() {
+        var cam = GetComponent<Camera>();
+        if(!cam) return;
+        var mode = cam.depthTextureMode;
+
+        if((mode & DepthTextureMode.DepthNormals) == DepthTextureMode.DepthNormals) {
+            Shader.EnableKeyword("CAMERA_DEPTHNORMALS");
+        }
+        else {
+            Shader.EnableKeyword("CAMERA_DEPTH");
+        }
+    }
 }
